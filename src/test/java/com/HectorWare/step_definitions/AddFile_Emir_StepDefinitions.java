226e5f5abeeb2b95ec18c0e2e02dd2512273b8fe
emirb <emirbeyazortu@gmail.com>
2022-07-11 22:04:42 +0100

    Emir Sprint 4

---
+++ src/test/java/com/HectorWare/step_definitions/AddFile_Emir_StepDefinitions.java
@@ -0,0 +1,84 @@
+package com.HectorWare.step_definitions;
+
+import com.HectorWare.pages.AddFile_EmirPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.awt.*;
+
+public class AddFile_Emir_StepDefinitions {
+
+    AddFile_EmirPage addFileEmirPage = new AddFile_EmirPage();
+
+    @Given("user is on the login page of the HectorWare Application")
+    public void user_is_on_the_login_page_of_the_hector_ware_application() {
+
+        addFileEmirPage.login();
+
+    }
+
+    @When("user click the plus button")
+    public void user_click_the_plus_button() {
+        addFileEmirPage.addFilePlusBtn.click();
+    }
+
+    @When("user upload a selected file up to Ten MB")
+    public void user_upload_a_selected_file_up_to_ten_mb() throws AWTException {
+
+        addFileEmirPage.upTo10MBFileUpload();
+
+    }
+
+
+    @Then("user should see the uploaded file on the screen")
+    public void user_should_see_the_uploaded_file_on_the_screen() {
+        Assert.assertTrue(addFileEmirPage.upTo10MBfileAudio.isDisplayed());
+    }
+
+
+
+    @Given("user upload valid image files")
+    public void user_upload_valid_image_files() throws AWTException {
+
+        addFileEmirPage.validImageFileUpload();
+
+    }
+    @Then("user should see the valid image files on the screen")
+    public void user_should_see_the_valid_image_files_on_the_screen() {
+        Assert.assertTrue(addFileEmirPage.validImageFile.isDisplayed());
+    }
+
+
+
+    @Given("user upload valid document files")
+    public void user_upload_valid_document_files() throws AWTException {
+        addFileEmirPage.validDocumentFileUpload();
+    }
+    @Then("user should see the valid document files on the screen")
+    public void user_should_see_the_valid_document_files_on_the_screen() {
+        Assert.assertTrue(addFileEmirPage.validDocumentFile.isDisplayed());
+    }
+
+
+    @Given("user upload valid audio files")
+    public void user_upload_valid_audio_files() throws AWTException {
+        addFileEmirPage.validAudioFileUpload();
+    }
+    @Then("user should see the valid audio files on the screen")
+    public void user_should_see_the_valid_audio_files_on_the_screen() {
+        Assert.assertTrue(addFileEmirPage.validAudioFile.isDisplayed());
+    }
+
+    @Given("user upload valid video files")
+    public void user_upload_valid_video_files() throws AWTException {
+        addFileEmirPage.validVideoFileUpload();
+    }
+    @Then("user should see the valid video files on the screen")
+    public void user_should_see_the_valid_video_files_on_the_screen() {
+        Assert.assertTrue(addFileEmirPage.validVideoFile.isDisplayed());
+    }
+
+
+}
